0f62517d51192992b70b4ea8115c677371cebf7d
mriberadev <mriberadev@local>
2026-04-29 04:49:49 +0200

    Implemented REST POST and PUT methods
    Implemented POST and PUT methods for 'exercises' endpoint.
    Added exception handling.
    Added custom validation messages.
    Added request DTO and corresponding mapper method.
    Empty Exercise.video and Exercise.image fields get turned into null at setter level.

--- src/main/java/com/wt/workouttracker/exercise/controller/ExerciseController.java
+++ src/main/java/com/wt/workouttracker/exercise/controller/ExerciseController.java
@@ -1,11 +1,14 @@
 package com.wt.workouttracker.exercise.controller;
 
+import com.wt.workouttracker.exercise.dto.ExerciseRequestDTO;
 import com.wt.workouttracker.exercise.dto.ExerciseResponseDTO;
+import com.wt.workouttracker.exercise.dto.validators.CreateExerciseValidationGroup;
 import com.wt.workouttracker.exercise.service.ExerciseService;
+import jakarta.validation.Valid;
+import org.hibernate.validator.constraints.UUID;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +24,23 @@ public class ExerciseController {
 	@GetMapping
 	public ResponseEntity<List<ExerciseResponseDTO>> getExercises() {
 		List<ExerciseResponseDTO> exerciseResponseDTOS = exerciseService.getExercises();
-		return ResponseEntity.ok().body(exerciseResponseDTOS);
+		return ResponseEntity.ok(exerciseResponseDTOS);
+	}
+
+	@PostMapping
+	public ResponseEntity<ExerciseResponseDTO> createExercise(
+			@Validated({CreateExerciseValidationGroup.class}) @RequestBody ExerciseRequestDTO exerciseRequestDTO) {
+		ExerciseResponseDTO exerciseResponseDTO = exerciseService.createExercise(exerciseRequestDTO);
+		return ResponseEntity.ok(exerciseResponseDTO);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<ExerciseResponseDTO> updateExercise(
+			@PathVariable String id,
+			@Valid @RequestBody ExerciseRequestDTO exerciseRequestDTO) {
+
+		ExerciseResponseDTO exerciseResponseDTO = exerciseService.updateExercise(id, exerciseRequestDTO);
+
+		return ResponseEntity.ok(exerciseResponseDTO);
 	}
 }
